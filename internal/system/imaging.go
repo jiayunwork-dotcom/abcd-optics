93b41d775aa21636416c65b1ced5e7a32eed764e
“jiayun” <jiayun_work@163.com>
2026-08-25 15:02:42 +0800

    fix: 物像共轭反解分子用 B+Ds'

--- internal/system/imaging.go
+++ internal/system/imaging.go
@@ -14,17 +14,12 @@ func ImageDistance(m matrix.Mat2, objectDistance float64) (float64, error) {
 	return -(m.A*objectDistance + m.B) / den, nil
 }
 
-func objectDistanceNumerator(m matrix.Mat2, imageDistance float64) float64 {
-	return m.A + m.C*imageDistance
-}
-
 func ObjectDistanceFor(m matrix.Mat2, imageDistance float64) (float64, error) {
 	den := m.C*imageDistance + m.A
 	if matrix.AlmostZero(den) {
 		return 0, fmt.Errorf("image plane at focal point: no finite object")
 	}
-	num := objectDistanceNumerator(m, imageDistance)
-	return -num / den, nil
+	return -(m.B + m.D*imageDistance) / den, nil
 }
 
 func BackFocalDistance(m matrix.Mat2) (float64, error) {
